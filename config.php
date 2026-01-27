3a586d0509c7a31bc218ea4980c0a6721f7273b7
deivsonltda <deivson.teclado@gmail.com>
2026-01-27 00:15:19 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
   'SUPABASE_ANON_KEY' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ibnBubWVteWxmanhqbnJocnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjgwMjIsImV4cCI6MjA4MjAwNDAyMn0.7v4y6O9jB6rripMQXkgY_wWIEfrysqWW8kpQ3pLYlpg',
   'SUPABASE_SERVICE_KEY' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ibnBubWVteWxmanhqbnJocnFuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NjQyODAyMiwiZXhwIjoyMDgyMDA0MDIyfQ.UdZw5M8kp4_gSraJ8H3Obb1l_2gNkEYPQSGrWN1DTik', // só admin/backend
   'WHATSAPP_NUMBER' => '5581936180732',
-  'BASE_URL' => 'https://gateway.streambrasil.online',
+  'BASE_URL' => 'https://streambrasil.online',
   'BASE_ADMIN' => 'https://admin.streambrasil.online',
   'BASE_AFF'   => 'https://afiliado.streambrasil.online',
   'BASE_CRM'   => 'https://crm.streambrasil.online',
